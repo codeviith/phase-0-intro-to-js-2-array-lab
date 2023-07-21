0378c323d104eb70308f1be0bb6194ba46f87371
codeviith <codeviith@Cinya>
2023-07-21 17:29:02 -0400

    JS Lab 7

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newLastCats = [];
+    return newLastCats = [...cats, name];
+}
+
+function prependCat(name) {
+    let newFirstCats = [];
+    return newFirstCats = [name, ...cats];
+}
+
+function removeLastCat() {
+    let newLastCats = [];
+    return cats.slice(0, cats.length -1);
+}
+
+function removeFirstCat() {
+    let newFirstCats = [];
+    return cats.slice(1);
+}
+
+
